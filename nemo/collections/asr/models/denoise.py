4e986937b0eac7e9aa8b6442cb11e066add29ca3
nghiahuynh-ai <huynhnguyenhieunghia1999@gmail.com>
2023-05-12 21:33:25 +0700

    update

--- nemo/collections/asr/models/denoise.py
+++ nemo/collections/asr/models/denoise.py
@@ -311,7 +311,7 @@ class Denoising(ModelPT, ASRModuleMixin, AccessMixin):
         ssim_score = ssim(denoised_spec.unsqueeze(1), clean_spec.unsqueeze(1))
         psnr_score = psnr(denoised_spec, clean_spec)
 
-        tensorboard_logs = {'val_loss': loss_value, 'ssim': ssim_score, 'psnr': psnr_score}
+        tensorboard_logs = {'val_loss': loss_value, 'ssim_score': ssim_score, 'psnr_score': psnr_score}
         self.log_dict(tensorboard_logs)
         
         return {'val_loss': loss_value, 'ssim': ssim_score, 'psnr': psnr_score, 'log': tensorboard_logs}
@@ -320,5 +320,5 @@ class Denoising(ModelPT, ASRModuleMixin, AccessMixin):
         val_loss_mean = torch.stack([x['val_loss'] for x in outputs]).mean()
         ssim_mean = torch.stack([x['ssim_score'] for x in outputs]).mean()
         psnr_mean = torch.stack([x['psnr_score'] for x in outputs]).mean()
-        tensorboard_logs = {'val_loss': val_loss_mean, 'ssim': ssim_mean, 'psnr': psnr_mean}
-        return {'val_loss': val_loss_mean, 'ssim': ssim_mean, 'psnr': psnr_mean, 'log': tensorboard_logs}
+        tensorboard_logs = {'val_loss': val_loss_mean, 'ssim_score': ssim_mean, 'psnr_score': psnr_mean}
+        return {'val_loss': val_loss_mean, 'ssim_score': ssim_mean, 'psnr_score': psnr_mean, 'log': tensorboard_logs}
