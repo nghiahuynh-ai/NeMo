54f5781af07fdb1b7bdce39114048fc0ec688ce6
nghiahuynh-ai <huynhnguyenhieunghia1999@gmail.com>
2023-05-22 17:01:29 +0700

    add ssl_reconstruct

--- nemo/collections/asr/parts/submodules/residual.py
+++ nemo/collections/asr/parts/submodules/residual.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 
 class Residual(nn.Module):
     def __init__(self, module: nn.Module):
+        super(Residual, self).__init__()
         self.module = module
         
     def forward(self, x):
